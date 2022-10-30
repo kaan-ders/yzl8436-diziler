29321770a56544cea83e39491ec7284942ad1627
Kaan Arslan <115875320+kaan-ders@users.noreply.github.com>
2022-10-30 14:27:17 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿namespace Diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //c# 3 tipte dizi yapısı
+
+            //diziler, array
+
+            /*
+             * 1) sabit bir boyutu vardır
+             * 2) aynı tipte olmalıdırlar
+             */
+
+            //string isim1, isim2, isim3, isim4;
+
+            //string isim = "ahmet";
+
+            //var a = isim[3];
+            //isim.Length
+
+            //string[] isimler = new string[3];
+
+            //isimler[0] = "ahmet";
+            //isimler[1] = "mehmet";
+            //isimler[2] = "ayşe";
+            //isimler[3] = 34;
+            //isimler[3] = "fatma";
+
+            /*for (int i = 0; i < isimler.Length; i++)
+            {
+                for (int j = isimler[i].Length - 1; j >= 0; j--)
+                {
+                    Console.Write(isimler[i][j]);
+                }
+
+                Console.WriteLine();
+            }*/
+
+
+            /*0        1         2
+            "ahmet", "mehmet", "ayşe"*/
+
+            //string isim = "ahmet ayşe fatma bilgeadam"; //dsa
+            //isim.Length
+            //isim[2] -d
+
+            //string deger = "", deger2 = "";
+            //for (int i = isim.Length - 1; i >= 0; i--)
+            //{
+            //    //deger += isim[i];
+            //    //deger2 = isim[i].ToString();
+            //    Console.Write(isim[i]);
+            //}
+
+            //for (int i = 1; i <= isim.Length; i++)
+            //{
+            //    Console.Write(isim[isim.Length - i]);
+            //}
+
+            //int[] sayilar = new int[10];
+            //sayilar[0] = 1;
+            //sayilar[1] = 2;
+
+            //bir önceki sayıyla toplamları
+            //int[] sayilar = { 1342, 21, 332, 4456, 57, 698, 790, 38, 19, 12 };
+            //int sonuc = 0;
+
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    /*if(i > 0)
+            //        Console.WriteLine(sayilar[i] + ": " + (sayilar[i] + sayilar[i - 1]));
+            //    else
+            //        Console.WriteLine(sayilar[i]);*/
+
+            //    //Console.WriteLine(i > 0 ? (sayilar[i] + sayilar[i - 1]) : sayilar[i]);
+
+            //    sonuc += sayilar[i];
+            //    Console.WriteLine(sayilar[i] + ": " + sonuc);
+            //}
+
+            /*"Kupa"
+            "Maça"
+            "Karo"
+            "Sinek"*/
+
+
+
+            /*"Kupa|1"
+            "Kupa|2"
+            "Kupa|3"
+            "Maça|1"
+            "Maça|2"*/
+
+            string[] deste = new string[52];
+            string[] kagit = { "Kupa", "Karo", "Maça", "Sinek" };
+
+            for (int i = 0; i < kagit.Length; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    deste[i * j] = kagit[i] + "|" + (j + 1);
+                }
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
